ec5ed3045e3cde13dd09f4fe302d8b4ce63eac4f
lcdnp00 <dionyssopouloselias@gmail.com>
2025-12-04 14:35:36 +0100

    fix déclaration de variable dans main.c

--- src/main.c
+++ src/main.c
@@ -37,6 +37,9 @@ int main(int argc, char *argv[]){
 	
 	else if (argc == 3)
 	{
+		char* op = argv[1];
+		char* a = argv[2];
+		double r = 0;
 		if (strcmp(op, "car") == 0)
 		{
 			r = _car(atof(a));
